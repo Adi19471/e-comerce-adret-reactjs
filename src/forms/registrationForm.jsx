df5653f36efe5268c60393029200bc16844946b8
ADI NARAYANA REDDY <akumatha@gmail.com>
2023-08-13 17:19:33 +0530

    Products added info data

--- src/forms/registrationForm.jsx
+++ src/forms/registrationForm.jsx
@@ -3,32 +3,37 @@ const registrationForm = [
     name: "Enter Your Name",
     label: "full name",
     type: "text",
+    placholder :"Enter Your Name",
+    required: "true",
+    isValid: "true",
   },
-
 
   {
     name: "Enter Your Email",
     label: "full email",
     type: "text",
+    placholder :"Enter Your Email Id",
+    required: "true",
+    isValid: "true",
   },
-
 
   {
     name: "Enter Secure Password",
     label: "password",
     type: "password",
+    placholder :"Enter Your Password",
+    required: "true",
+    isValid: "true",
   },
-
 
   {
     name: "Enter again Secure Password",
     label: "cpassword",
     type: "password",
+    placholder :"Enter Again Password",
+    required: "true",
+    isValid: "true",
   },
-
-
-
 ];
-
 
 export default registrationForm;
--- src/pages/Registationpage.jsx
+++ src/pages/Registationpage.jsx
@@ -1,35 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
 import registrationForm from "../forms/registrationForm";
 
-import { Form, FormGroup, Label, Input, Button, } from "reactstrap";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 
 const Registationpage = () => {
+
+const[regFormJson ,setRegForm] = useState(registrationForm)
+
+const validate = (event) =>{
+  if(event.target.value.length === 0){
+    const formJson = regFormJson
+    formJson.forEach((item) =>{
+      if(event.target.name === item.name){
+        item.isValid = false;
+      }
+    })
+
+    setRegForm([...formJson])
+  }
+}
+
+
   return (
     <>
       <Form>
-
-      <h2 >Registratioon Aplication...</h2>
+        <h2>Registratioon Aplication...</h2>
         <FormGroup>
-          {registrationForm &&
-            registrationForm.map((item, index) => {
-              return ( 
+          {registrationForm && registrationForm.length > 0 && 
+            regFormJson.map((item, index) => {
+              const {isValid} = item;
+              return (
                 <>
-      <div key={index}>
-      <Label htmlFor="">{item.name}</Label>
-                  <Input label={item.label} type={item.type} />
-      </div>
+                  <div key={index}>
+                    <Label htmlFor="">{item.name}</Label>
+                    {
+                      isValid && <Input  valid label={item.label}  name={item.name} type={item.type}  placeholder={item.placholder}  onBlur={validate}/>
+                    }
+
+                    {
+                      !isValid && <Input invalid label={item.label}   name={item.name}   type={item.type}  placeholder={item.placholder}  onBlur={validate}/>
+                    }
+                    
+                  </div>
                 </>
               );
             })}
 
-     
-            <Button color="primary" size="sm">
+          <Button color="primary" size="sm">
             Submit
           </Button>
-          <Button color="info" size="sm">cancel</Button>
-
-     
+          <Button color="info" size="sm">
+            cancel
+          </Button>
         </FormGroup>
       </Form>
     </>
